26e2c14254c2db13fb67d7d18d1402600ed499e6
unknown <hassanoubella077@gmail.com>
2022-10-07 19:12:19 +0100

    iniat commit

---
+++ js TP/script.js
@@ -0,0 +1,22 @@
+var img=document.querySelector("#began");
+var sllimg=document.querySelectorAll(".slamm");
+
+sllimg[0].onclick =function() {
+img.src=sllimg[0].src;
+
+     }                       
+sllimg[1].onclick =function() {
+    img.src=sllimg[1].src;
+                              }
+
+sllimg[2].onclick =function() {
+        img.src=sllimg[2].src;
+        }
+
+sllimg[3].onclick =function() {
+            img.src=sllimg[3].src;
+            }
+
+ sllimg[4].onclick =function() {
+                img.src=sllimg[4].src;
+                }
